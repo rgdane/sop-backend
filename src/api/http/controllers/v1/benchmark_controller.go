3d1e99f4077464ef08dbf32999d5c68b98ae3f07
rgdane <regadanew1@gmail.com>
2026-06-28 10:38:38 +0700

    [Update][Benchmark] Dynamic request rate and duration

--- src/api/http/controllers/v1/benchmark_controller.go
+++ src/api/http/controllers/v1/benchmark_controller.go
@@ -18,80 +18,110 @@ type BenchmarkResult struct {
 
 func RunBenchmarkSopJobTitleSQL(cn *container.AppContainer) fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		rate := c.QueryInt("rate", 100)
+		duration := c.QueryInt("duration", 5)
+
 		targetURL := "http://localhost:5000/api/v1/sop-jobs/title-sql/?title_name=Engineer"
-		result := helper.RunVegetaLoadTest("GET", targetURL, "SQL - SOP Jobs by Title", 5, 20)
+		result := helper.RunVegetaLoadTest("GET", targetURL, "SQL - SOP Jobs by Title", duration, rate)
 		return c.JSON(fiber.Map{"message": "Benchmark SQL SOP Jobs by Title selesai", "data": result})
 	}
 }
 
 func RunBenchmarkSopJobTitleGraph(cn *container.AppContainer) fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		rate := c.QueryInt("rate", 100)
+		duration := c.QueryInt("duration", 5)
+
 		targetURL := "http://localhost:5000/api/v1/sop-jobs/title-graph/?title_name=Engineer"
-		result := helper.RunVegetaLoadTest("GET", targetURL, "Graph - SOP Jobs by Title", 5, 20)
+		result := helper.RunVegetaLoadTest("GET", targetURL, "Graph - SOP Jobs by Title", duration, rate)
 		return c.JSON(fiber.Map{"message": "Benchmark Graph SOP Jobs by Title selesai", "data": result})
 	}
 }
 
 func RunBenchmarkSopJobDivisionSQL(cn *container.AppContainer) fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		rate := c.QueryInt("rate", 100)
+		duration := c.QueryInt("duration", 5)
+
 		targetURL := "http://localhost:5000/api/v1/sop-jobs/division-sql/?division_name=Product"
-		result := helper.RunVegetaLoadTest("GET", targetURL, "SQL - SOP Jobs by Division", 5, 20)
+		result := helper.RunVegetaLoadTest("GET", targetURL, "SQL - SOP Jobs by Division", duration, rate)
 		return c.JSON(fiber.Map{"message": "Benchmark SQL SOP Jobs by Division selesai", "data": result})
 	}
 }
 
 func RunBenchmarkSopJobDivisionGraph(cn *container.AppContainer) fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		rate := c.QueryInt("rate", 100)
+		duration := c.QueryInt("duration", 5)
+
 		targetURL := "http://localhost:5000/api/v1/sop-jobs/division-graph/?division_name=Product"
-		result := helper.RunVegetaLoadTest("GET", targetURL, "Graph - SOP Jobs by Division", 5, 20)
+		result := helper.RunVegetaLoadTest("GET", targetURL, "Graph - SOP Jobs by Division", duration, rate)
 		return c.JSON(fiber.Map{"message": "Benchmark Graph SOP Jobs by Division selesai", "data": result})
 	}
 }
 
 func RunBenchmarkSopJobDivisionTitleSQL(cn *container.AppContainer) fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		rate := c.QueryInt("rate", 100)
+		duration := c.QueryInt("duration", 5)
+
 		targetURL := "http://localhost:5000/api/v1/sop-jobs/division-title-sql/?division_name=Product&title_name=Engineer"
-		result := helper.RunVegetaLoadTest("GET", targetURL, "SQL - SOP Jobs by Division & Title", 5, 20)
+		result := helper.RunVegetaLoadTest("GET", targetURL, "SQL - SOP Jobs by Division & Title", duration, rate)
 		return c.JSON(fiber.Map{"message": "Benchmark SQL SOP Jobs by Division & Title selesai", "data": result})
 	}
 }
 
 func RunBenchmarkSopJobDivisionTitleGraph(cn *container.AppContainer) fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		rate := c.QueryInt("rate", 100)
+		duration := c.QueryInt("duration", 5)
+
 		targetURL := "http://localhost:5000/api/v1/sop-jobs/division-title-graph/?division_name=Product&title_name=Engineer"
-		result := helper.RunVegetaLoadTest("GET", targetURL, "Graph - SOP Jobs by Division & Title", 5, 20)
+		result := helper.RunVegetaLoadTest("GET", targetURL, "Graph - SOP Jobs by Division & Title", duration, rate)
 		return c.JSON(fiber.Map{"message": "Benchmark Graph SOP Jobs by Division & Title selesai", "data": result})
 	}
 }
 
 func RunBenchmarkSopJobReferenceDivisionSQL(cn *container.AppContainer) fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		rate := c.QueryInt("rate", 100)
+		duration := c.QueryInt("duration", 5)
+
 		targetURL := "http://localhost:5000/api/v1/sop-jobs/reference-division-sql/?reference_division_name=Product"
-		result := helper.RunVegetaLoadTest("GET", targetURL, "SQL - SOP Jobs by Reference Division", 5, 20)
+		result := helper.RunVegetaLoadTest("GET", targetURL, "SQL - SOP Jobs by Reference Division", duration, rate)
 		return c.JSON(fiber.Map{"message": "Benchmark SQL SOP Jobs by Reference Division selesai", "data": result})
 	}
 }
 
 func RunBenchmarkSopJobReferenceDivisionGraph(cn *container.AppContainer) fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		rate := c.QueryInt("rate", 100)
+		duration := c.QueryInt("duration", 5)
+
 		targetURL := "http://localhost:5000/api/v1/sop-jobs/reference-division-graph/?reference_division_name=Product"
-		result := helper.RunVegetaLoadTest("GET", targetURL, "Graph - SOP Jobs by Reference Division", 5, 20)
+		result := helper.RunVegetaLoadTest("GET", targetURL, "Graph - SOP Jobs by Reference Division", duration, rate)
 		return c.JSON(fiber.Map{"message": "Benchmark Graph SOP Jobs by Reference Division selesai", "data": result})
 	}
 }
 
 func RunBenchmarkSopJobDivisionTitlePublishedSQL(cn *container.AppContainer) fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		rate := c.QueryInt("rate", 100)
+		duration := c.QueryInt("duration", 5)
+
 		targetURL := "http://localhost:5000/api/v1/sop-jobs/division-title-published-sql/?division_name=Product&job_name_pattern=mengoptimalkan&title_color=%2333FF57"
-		result := helper.RunVegetaLoadTest("GET", targetURL, "SQL - SOP Jobs by Division/Title/Published", 5, 20)
+		result := helper.RunVegetaLoadTest("GET", targetURL, "SQL - SOP Jobs by Division/Title/Published", duration, rate)
 		return c.JSON(fiber.Map{"message": "Benchmark SQL SOP Jobs by Division/Title/Published selesai", "data": result})
 	}
 }
 
 func RunBenchmarkSopJobDivisionTitlePublishedGraph(cn *container.AppContainer) fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		rate := c.QueryInt("rate", 100)
+		duration := c.QueryInt("duration", 5)
+
 		targetURL := "http://localhost:5000/api/v1/sop-jobs/division-title-published-graph/?division_name=Product&job_name_pattern=mengoptimalkan&title_color=%2333FF57"
-		result := helper.RunVegetaLoadTest("GET", targetURL, "Graph - SOP Jobs by Division/Title/Published", 5, 20)
+		result := helper.RunVegetaLoadTest("GET", targetURL, "Graph - SOP Jobs by Division/Title/Published", duration, rate)
 		return c.JSON(fiber.Map{"message": "Benchmark Graph SOP Jobs by Division/Title/Published selesai", "data": result})
 	}
 }
